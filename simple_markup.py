cf9752d2138e6d60861ed196a9d97a908fae9a1e
tunzao <l.tunzao@gmail.com>
2014-02-13 19:16:10 +0800

    add three files

---
+++ simple_markup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import re
+from util import *
+
+print '<html><title>...</title><body>'
+
+title = True
+for block in blocks(sys.stdin):
+    block = re.sub(r'\*(.+?)\*', r'<em>\1</em>', block)
+    if title:
+        print '<h1>'
+        print block
+        print '</h1>'
+        title = False
+    else:
+        print '<p>'
+        print block
+        print '</p>'
+
+print '</body></html>'
+
