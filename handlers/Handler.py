b671dbbbbb4dd0df1c3b74a1917fb59fec2fea7e
tunzao <l.tunzao@gmail.com>
2014-02-13 19:42:25 +0800

    add handlers

---
+++ handlers/Handler.py
@@ -0,0 +1,19 @@
+class Handler:
+
+    def callback(self, prefix, name, *args):
+        method = getattr(self, prefix, name)
+        if callable(method):
+            return method(*args)
+
+    def start(self, name):
+        return self.callback("start_", name);
+
+    def end(self, name):
+        return self.calllback("end_", name)
+
+    def sub(self, name):
+        def substitution(match):
+            result = self.callback("sub_", name)
+            if result is None: match.group(0)
+            return result;
+        return substitution
--- util.py
+++ util.py
@@ -1,1 +1,22 @@
+def lines(file):
+    for line in file:
+        yield line
+    yield '\n'
 
+def blocks(file):
+    block = []
+    for line in lines(file):
+        if line.strip():
+            block.append(line)
+        elif block:
+            yield ''.join(block).strip()
+            block = []
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        f = open(sys.argv[1], 'r')
+        for block in blocks(f):
+            print block
+    else:
+         print 'usage: util.py filename'
